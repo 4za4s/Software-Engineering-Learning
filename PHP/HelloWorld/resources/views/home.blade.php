9eea8e7bd19950fbc676631f4766d3719c2e7fd8
4za4s <arbo3802@gmail.com>
2026-03-03 21:26:45 +1100

    Messing around with laravel and the directory structure

---
+++ PHP/HelloWorld/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<html>
+
+<head></head>
+
+<body>
+    <p><a href="/helloworld">link text</a></p>
+    <?php ?>
+</body>
+
+</html>
--- PHP/HelloWorld/routes/web.php
+++ PHP/HelloWorld/routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', view('home'));
+Route::view('/helloworld', view('helloworld'));
